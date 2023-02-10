5f7254da07e8f3ab33cef3d50a305a67e93afb84
Mendrika <mendrikaandriamitantsoa@gmail.com>
2023-02-10 11:12:20 +0300

    Update settings.py

--- src/main/settings.py
+++ src/main/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'fk@ih--639=j%8x&e(7ukn-=!^81mkryt)u1^d_+yfh+5i8s(&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['appdev.mg']
 
 # Application definition
 
