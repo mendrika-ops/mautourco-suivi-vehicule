253f5bb617d6b11cfda40ec5cee152472c65d509
Mendrika <mendrikaandriamitantsoa@gmail.com>
2023-02-14 11:30:51 +0300

    append slash in settigns

--- src/main/settings.py
+++ src/main/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
+APPEND_SLASH = False
+
 USERIDGUID = '3f55ba57-8a28-41cc-917d-718b9a754410'
 SESSIONID = 'e3f43a87-0386-4928-bcbb-2791b54b0b64'
 JOB_MINUTE = 30
