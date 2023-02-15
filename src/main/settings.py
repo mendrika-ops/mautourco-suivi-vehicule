806a35b7004ef356b7e0abf7df4a814deb82b78e
Mendrika <mendrikaandriamitantsoa@gmail.com>
2023-02-15 12:30:55 +0300

    Update settings.py

--- src/main/settings.py
+++ src/main/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 APPEND_SLASH = False
 
 USERIDGUID = '3f55ba57-8a28-41cc-917d-718b9a754410'
-SESSIONID = 'e3f43a87-0386-4928-bcbb-2791b54b0b64'
+SESSIONID = 'bf28ce2c-227e-401f-ab65-87b536fdeba7'
 JOB_MINUTE = 5
 
 CRONJOBS = [
