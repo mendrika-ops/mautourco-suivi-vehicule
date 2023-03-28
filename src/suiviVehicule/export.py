5d05e4a2f01d578f6d264182f02e05366df6c127
Mendrika <77017214+mendrika-ops@users.noreply.github.com>
2023-03-28 15:50:55 +0300

    fix nombre and add export excel

---
+++ src/suiviVehicule/export.py
@@ -0,0 +1,25 @@
+
+from django.db import models
+
+class Recordexport(models.Model):
+    id = models.indexes
+    id_trip = models.IntegerField(null=False)
+    comment = models.TextField(max_length=500)
+    vehicleno = models.CharField(max_length=100)
+    driver_oname = models.CharField(max_length=150)
+    FromPlace = models.CharField(max_length=150)
+    ToPlace = models.CharField(max_length=150)
+    trip_start_date = models.CharField(max_length=15, null=False)
+    pick_up_time = models.CharField()
+    status = models.CharField(max_length=100, null=True)
+    couleur = models.CharField(max_length=100, null=True)
+    daterecord = models.DateField()
+    etat = models.IntegerField(default=0)
+    driver_mobile_number = models.CharField(max_length=50)
+    current = models.CharField(max_length=150,null=True)
+    actualtime = models.CharField()
+    difftimestart = models.FloatField(null=True)
+    difftimepickup = models.FloatField(null=True)
+    class Meta:
+        managed = False
+        db_table = 'suivivehicule_recordtrajet_export'
