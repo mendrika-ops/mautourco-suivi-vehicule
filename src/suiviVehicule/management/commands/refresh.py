a93e401ae27a43e2645037fde780f58aa5a5b5f8
Mendrika <77017214+mendrika-ops@users.noreply.github.com>
2023-06-20 13:49:29 +0300

    Revert "Merge branch 'develop'"
    This reverts commit 64b74c4ea8fea8ef06cc7d58c34c452958bb9553, reversing
    changes made to 503cae7d5a2eb842ab58fbdb395d21f9d24cf44f.

--- src/suiviVehicule/management/commands/refresh.py
+++ src/suiviVehicule/management/commands/refresh.py
@@ -1,19 +1,8 @@
-import sys
 from django.core.management.base import BaseCommand, CommandError
 from suiviVehicule.services import services
-import subprocess
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-        try:
-            subprocess.call('taskkill /IM cmd.exe /F', shell=True)
-            services().refresh()
-            print("themmmmmeeeeeeeeeeeeeeeeeeee")
-            self.exit_code = 1
-            self.handle_internal(*args, **kwargs)
-            sys.exit(self.exit_code)
-        except Exception:
-            self.exit_code = 0
-            self.handle_internal(*args, **kwargs)
-            sys.exit(self.exit_code)
+    def handle(self, *args, **options):
+        services().refresh()
+        print("themmmmmeeeeeeeeeeeeeeeeeeee")
         return 
